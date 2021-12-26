b24e0d86d6da244d51b0e8c20f3cbcc5c0533ed5
Alenka53 <77922124+Alenka53@users.noreply.github.com>
2021-12-26 18:22:29 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,30 @@
+import csv
+import sys
 
+
+def main():
+
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python task1.py massive_lenth_argument step_lenth_argument")
+
+    n = int(sys.argv[1])
+    m = int(sys.argv[2])
+
+    steps = []
+
+    steps.append(1)
+    l = 1
+    a = 0
+    while a != 1:
+        if l+m-1 <= n:
+            l = l+m-1
+            steps.append(l)
+            a = l
+            print(a)
+        elif l+m-1 > n:
+            l = l - n + m - 1
+            steps.append(l)
+            a = l
+            print(a)
+
+    print (*steps)
